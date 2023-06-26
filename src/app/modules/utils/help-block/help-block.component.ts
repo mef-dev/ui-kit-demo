e237b6bcd2802b3eca97f5dfa0027e612b105183
Maria Shahrai <Maria.Shahrai@mef.dev>
2023-06-26 18:40:48 +0300

    update

---
+++ src/app/modules/utils/help-block/help-block.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { MefDevHelpBlockModule } from '@natec/mef-dev-ui-kit';
+
+@Component({
+  selector: 'app-help-block',
+  templateUrl: './help-block.component.html',
+  styleUrls: ['./help-block.component.scss']
+})
+export class HelpBlockComponent implements OnInit {
+  isVisible:boolean = true;
+  expanded:boolean = false;
+
+  @ViewChild('title') title:string = "Help";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
